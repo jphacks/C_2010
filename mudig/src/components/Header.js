a93a52ccc1874e4b2422a2b58eafd42308458590
obashun22 <obashun22@yahoo.co.jp>
2020-11-05 19:51:03 +0900

    [fix] ファイル名修正

---
+++ mudig/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../css/components/header.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <div className="header">
+          <div className="symbol">Mudig</div>
+          <div className="menu"><i className="fas fa-bars fa-2x"></i></div>
+        </div>
+        <div className="add-margin-top"></div>
+      </header>
+    )
+  };
+}
+
+export default Header;
