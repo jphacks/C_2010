9f4b1f7e00409eaafae80d83dc76faa3cb079e66
hide <nkret805worm@gmail.com>
2020-11-07 06:36:34 +0900

    ハンドリング

--- mudig/src/api/fierbase/getfiredata.js
+++ mudig/src/api/fierbase/getfiredata.js
@@ -16,22 +16,22 @@ async function getMusicData(id, type){
     console.log("index=" + index)
     switch (type) {
       case "title":
-        return data[index].music.title;
+        return data[index]?.music.title;
         break;
       case "artist":
-        return data[index].music.artist;
+        return data[index]?.music.artist;
         break;
       case "comment":
-        return data[index].music.comment;
+        return data[index]?.music.comment;
         break;
       case "start_time":
-        return data[index].music.start_time;
+        return data[index]?.music.start_time;
         break;
       case "finish_time":
-        return data[index].music.finish_time;
+        return data[index]?.music.finish_time;
         break;
       case "url":
-        return data[index].music.url;
+        return data[index]?.music.url;
         break;
       case "length":
         return data.length;
