e49ec12043258c2b70a83adaf498c2f16c824c1b
hide <nkret805worm@gmail.com>
2020-11-07 05:36:50 +0900

    データ連携させた

--- mudig/src/components/FavCard.js
+++ mudig/src/components/FavCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../css/components/favCard.css';
-import GetMusicData from '../fierbase/getfiredata';
+import getMusicData from '../api/fierbase/getfiredata';
 import YoutubeFrame from '../api/youtube/youtubeFrame';
 
 class FavCard extends React.Component {
@@ -8,23 +8,40 @@ class FavCard extends React.Component {
     super(props);
     this.state = {
       id: props.id,
-      url: null,
-      title: null,
-      artist: null
+      url: '',
+      title: '',
+      artist: ''
     };
   };
+  async componentWillMount(){
+    console.log("called willMount");
+    this.setState({
+      url: await getMusicData(this.state.id, "url"),
+      title: await getMusicData(this.state.id, "title"),
+      artist: await getMusicData(this.state.id, "artist")
+    });
+  };
   render() {
+    console.log(this.state.url);
+    console.log(this.state.title);
     return (
+      <>
+      { this.state.url ? (
       <div className="music-card">
         <YoutubeFrame src={this.state.url} edge="150" />
         <div className="music-info">
-          <h3 className="music-name">{this.state.title}</h3>
-          <h5 className="artist-name">{this.state.artist}</h5>
+          <div className="nameWrapper">
+            <h3 className="music-name">{this.state.title}</h3>
+            <h5 className="artist-name">{this.state.artist}</h5>
+          </div>
           <div className="like">
             <i className="fas fa-heart fa-lg"></i>
           </div>
         </div>
       </div>
+
+      ) : null }
+      </>
     );
   };
 }
