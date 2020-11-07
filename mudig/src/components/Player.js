30ef4b621667e3bd38ab2bf0b93fd8ab64560e58
obashun22 <obashun22@yahoo.co.jp>
2020-11-07 12:49:08 +0900

    [update] seekbarの実装

--- mudig/src/components/Player.js
+++ mudig/src/components/Player.js
@@ -10,7 +10,7 @@ class Player extends React.Component {
     this.state = {
       liked: false,
       // 再生位置を管理
-      playingRate: 30,
+      playingRate: 0,
     }
   }
   toggleLike() {
@@ -33,6 +33,15 @@ class Player extends React.Component {
     // 右の矢印を押された時の実装
     console.log("tapped right button");
   }
+  tappedSeekBar(e) {
+    const zeroPos = e.target.getBoundingClientRect().left;
+    const seekbarLen = e.target.getBoundingClientRect().right - zeroPos;
+    const tappedPos = e.clientX;
+    const diffPos = tappedPos - zeroPos;
+    const playingRate = (diffPos / seekbarLen) * 100;
+    console.log(playingRate);
+    this.setState({ playingRate: playingRate });
+  }
   render() {
     const likeCls = this.state.liked ? "far fa-heart fa-2x" : "fas fa-heart fa-2x";
     const likeStyle = this.state.liked ? {"color": "#333"} : {"color": "red"};
@@ -44,8 +53,8 @@ class Player extends React.Component {
           </div>
           <div className="jacket-wrapper">
             <div className="jacket">
-              <img className="jacket-img" src={jacketImage} />
-              <div className="seekbar">
+              <img className="jacket-img" src={ jacketImage } />
+              <div className="seekbar" onClick={ (e) => this.tappedSeekBar(e) }>
                 <div className="play-pos" style={{"width": `${ this.state.playingRate }%`}}></div>
               </div>
             </div>
